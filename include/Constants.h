41e9bf84c07c5f02d2a52c3068552369eafb1cc4
VuzLe <tle63917@gmail.com>
2026-05-30 15:52:02 +0700

    feat: Add Constants header file for vault directory and file paths

---
+++ include/Constants.h
@@ -0,0 +1,9 @@
+#pragma once
+#include <string>
+
+namespace Constants {
+    inline const std::string VAULT_DIR = ".lptv";
+    inline const std::string VAULT_DB = ".lptv/vault.db";
+    inline const std::string MASTER_FILE = ".lptv/lptv.dat";
+    inline const std::string CONFIG_FILE = ".lptv/config.json";
+}
