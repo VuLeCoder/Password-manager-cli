193d56d70ed530a91ea27971205a8a790a3aacc0
VuzLe <tle63917@gmail.com>
2026-05-30 22:51:21 +0700

    feat: Enhance Storage class to encrypt and decrypt account data during save and load operations

--- src/Storage.cpp
+++ src/Storage.cpp
@@ -1,8 +1,11 @@
 #include "./../include/Storage.h"
+#include "./../include/Encryptor.h"
 
 #include <fstream>
 #include <sstream>
 #include <filesystem>
+
+const std::string KEY = "lptvu.vuzle";
 
 static std::vector<std::string> split(const std::string& str, char delimiter) {
     std::vector<std::string> result;
@@ -31,14 +34,14 @@ bool Storage::save(
         return false;
     }
 
+    std::string data = "";
     for(const auto& account : accounts) {
-        file
-            << account.getService() << "|"
-            << account.getUsername() << "|"
-            << account.getPassword() << "|"
-            << account.getNote()
-            << "\n";
+        data += account.getService() + "|"
+            + account.getUsername() + "|"
+            + account.getPassword() + "|"
+            + account.getNote() + "\n";
     }
+    file << Encryptor::encrypt(data, KEY);
 
     file.close();
     return true;
@@ -54,19 +57,37 @@ std::vector<Account> Storage::load(
         return accounts;
     }
 
+    std::stringstream buffer;
+    buffer << file.rdbuf();
+    file.close();
+
+    std::string encryted = buffer.str();
+    std::string plaintext = Encryptor::decrypt(encryted, KEY);
+
+    std::stringstream ss(plaintext);
     std::string line;
-    while(std::getline(file, line)) {
-        if(line.empty()) continue;
+    while(std::getline(ss, line)) {
+        if(line.empty()) {
+            continue;
+        }
 
         auto parts = split(line, '|');
-        if(parts.size() != 4) continue;
+
+        if(parts.size() == 3) {
+            parts.emplace_back("");
+        }
+
+        if(parts.size() != 4) {
+            continue;
+        }
 
         accounts.emplace_back(
-            parts[0], parts[1],
-            parts[2], parts[3]
+            parts[0],
+            parts[1],
+            parts[2],
+            parts[3]
         );
     }
-
-    file.close();
+    
     return accounts;
 }
