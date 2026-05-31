1b4a8cfcd9dd4bd7d64604201e5253d3b503079f
VuzLe <tle63917@gmail.com>
2026-05-31 12:06:36 +0700

    feat: Refactor file path constants to use VAULT_DIR for improved maintainability

--- include/utils/Constants.h
+++ include/utils/Constants.h
@@ -3,11 +3,14 @@
 
 namespace Constants {
     inline const std::string VAULT_DIR = ".lptv";
-    inline const std::string VAULT_DB = ".lptv/vault.db";
-    inline const std::string CATEGORY_DB = ".lptv/categories.db";
-    inline const std::string MASTER_FILE = ".lptv/lptv.dat";
-    inline const std::string CONFIG_FILE = ".lptv/config.json";
-    inline const std::string SESSION_FILE = ".lptv/session.dat";
+
+    inline const std::string VAULT_DB = VAULT_DIR + "/vault.db";
+    inline const std::string CATEGORY_DB = VAULT_DIR + "/categories.db";
+
+    inline const std::string MASTER_FILE = VAULT_DIR + "/lptv.dat";    
+    inline const std::string SESSION_FILE = VAULT_DIR + "/session.dat";
+    
+    inline const std::string CONFIG_FILE = VAULT_DIR + "/config.json";
 
     inline constexpr int SESSION_TIMEOUT = 1800;
 }
