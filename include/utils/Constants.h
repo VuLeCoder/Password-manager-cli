e41a80e316ffaf2b93c200c3f7d87d0afa4a5b94
VuzLe <tle63917@gmail.com>
2026-07-06 09:28:32 +0700

    feat: change std::string -> std::filesystem for filepath used

--- include/utils/Constants.h
+++ include/utils/Constants.h
@@ -1,14 +1,16 @@
 #pragma once
-#include <string>
+#include <filesystem>
+
+namespace fs = std::filesystem;
 
 namespace Constants {
-    inline const std::string VAULT_DIR = ".lptv";
+    inline const fs::path VAULT_DIR = ".lptv";
 
-    inline const std::string ACCOUNT = "accounts";
-    inline const std::string CATEGORY = "categories";
-    inline const std::string VAULT_DB = VAULT_DIR + "/vault.db";
+    inline const fs::path ACCOUNT = "accounts";
+    inline const fs::path CATEGORY = "categories";
+    inline const fs::path VAULT_DB = VAULT_DIR / "/vault.db";
 
-    inline const std::string MASTER_FILE = VAULT_DIR + "/lptv.dat";    
+    inline const fs::path MASTER_FILE = VAULT_DIR / "/lptv.dat";    
     
-    inline const std::string CONFIG_FILE = VAULT_DIR + "/config.json";
+    inline const fs::path CONFIG_FILE = VAULT_DIR / "/config.json";
 }
