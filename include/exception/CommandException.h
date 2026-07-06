f4e02e8ee8d8fdc786e2d1cfc3d1fd9dd93e5809
VuzLe <tle63917@gmail.com>
2026-07-07 00:55:55 +0700

    feat(exception): add CommandException class for handling command-related errors

---
+++ include/exception/CommandException.h
@@ -0,0 +1,28 @@
+#pragma once
+
+#include "PasswordManagerException.h"
+
+enum class CommandCode{
+    Unknown,
+    Invalid,
+};
+
+class CommandException : public PasswordManagerException {
+private:
+    CommandCode code_;
+
+public:
+    explicit CommandException(
+        CommandCode code, 
+        const std::string& message
+    ) : PasswordManagerException(message), code_(code) {}
+
+    explicit CommandException(
+        CommandCode code,
+        const char* message
+    ) : PasswordManagerException(message), code_(code) {}
+
+    CommandCode code() const noexcept {
+        return code_;
+    }
+};
