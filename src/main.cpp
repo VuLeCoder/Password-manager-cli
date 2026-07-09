0ef189e08320a27f5b962b55e772dbae2ca9c9a4
VuzLe <tle63917@gmail.com>
2026-07-09 10:01:22 +0700

    fix(main): improve error handling and include missing headers

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,20 @@
 #include "cli/CommandDispatcher.h"
 #include "cli/CommandParser.h"
+#include "cli/Console.h"
 
-int main(int argc, char* argv[]){
-    auto command = CommandParser::parse(argc, argv);
-    CommandDispatcher::execute(command);
+#include "exception/PasswordManagerException.h"
+
+int main(int argc, char* argv[]) {
+    try {
+        auto command = CommandParser::parse(argc, argv);
+        CommandDispatcher::execute(command);
+    } catch (const PasswordManagerException& e) {
+        Console::printError(e.what());
+        return 1;
+    } catch (const std::exception& e) {
+        Console::printError(e.what());
+        return 1;
+    }
     
     return 0;
 }
